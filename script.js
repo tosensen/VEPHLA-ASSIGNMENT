0aa4eb4acbb335f545e50ee3584bdb61ccb9f7d9
Adedapo Oluwatosin <janetoluwatosin2015@gmail.com>
2022-09-22 00:11:22 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+script.js
+Who has access
+Not shared
+System properties
+Type
+JavaScript
+Size
+801 bytes
+Storage used
+801 bytes
+Location
+website
+Owner
+me
+Modified
+31 Aug 2022 by me
+Opened
+12:50 by me
+Created
+31 Aug 2022 with ZIP Extractor
+Add a description
+Viewers can download
+const delay = 3000; //ms
+
+const slides = document.querySelector(".slides");
+const slidesCount = slides.childElementCount;
+const maxLeft = (slidesCount - 1) * 100 * -1;
+
+let current = 0;
+
+function changeSlide(next = true) {
+  if (next) {
+    current += current > maxLeft ? -100 : current * -1;
+  } else {
+    current = current < 0 ? current + 100 : maxLeft;
+  }
+
+  slides.style.left = current + "%";
+}
+
+let autoChange = setInterval(changeSlide, delay);
+const restart = function() {
+  clearInterval(autoChange);
+  autoChange = setInterval(changeSlide, delay);
+};
+
+// Controls
+document.querySelector(".next-slide").addEventListener("click", function() {
+  changeSlide();
+  restart();
+});
+
+document.querySelector(".prev-slide").addEventListener("click", function() {
+  changeSlide(false);
+  restart();
+});
